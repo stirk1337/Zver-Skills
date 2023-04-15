6806dab300e23a49e23395942a52d808d0858674
Ramil Mavlutov <stirk2281337@gmail.com>
2023-04-16 03:11:34 +0500

    xd

--- SberAcademy/vacancy/views.py
+++ SberAcademy/vacancy/views.py
@@ -29,7 +29,31 @@ def get_vacancies(request):
 
 def test_vac(request):
     vac = Vacancy.objects.get(id=request.GET.get('vacancy_id'))
-    return JsonResponse(model_to_dict(vac))
+    skills = vac.skill_set.all()
+    q_list = []
+    test_xd = Test(name='На вакансию ' + vac.name, random=True, skill='Net', description='Тест оценит ваши знания на вакансию ' + vac.name )
+    test_xd.save()
+    for skill in skills:
+        tests = Test.objects.all().filter(skill=skill.skill)
+        for test in tests:
+            questions = test.question_set.all()
+            for question in questions:
+                q_list.append({'question': question.question, 'opt1': question.opt1, 'opt2': question.opt2, 'opt3': question.opt3, 'opt4': question.opt4, 'right': question.right, 'skill': skill.skill})
+    
+    random_list = random.sample(q_list, min(len(skills)*10, len(q_list)))
+    for q in random_list:
+        question = Question(question=q['question'],
+                            opt1=q['opt1'],
+                            opt2=q['opt2'],
+                            opt3=q['opt3'],
+                            opt4=q['opt4'],
+                            right=q['right'],
+                            skill=q['skill'],
+                            test=test_xd)
+        question.save()
+    test_dict = model_to_dict(test_xd)
+    test_dict['questions'] = random_list
+    return JsonResponse(test_dict)
 
 def test(request, vacancy_id):
     return render(request, 'vacancy/vac_test.html')
