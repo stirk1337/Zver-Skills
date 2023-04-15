ab77990140a9c5afadad7e8c9ccf86270d3d7a50
Ramil Mavlutov <stirk2281337@gmail.com>
2023-04-16 02:44:21 +0500

    xd

--- SberAcademy/vacancy/urls.py
+++ SberAcademy/vacancy/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('test', views.test),
     path('start_test', views.start_test),
     path('complete_test', views.complete_test),
-    path('test/<int:test_id>', views.test)
+    path('test/<int:vacancy_id>', views.test)
 ]
