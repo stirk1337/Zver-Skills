1d4000a0688cfb60fb2b6858c83651a78b1074fa
Serkik <63666583+Serckik@users.noreply.github.com>
2023-04-15 20:34:06 +0500

    test

---
+++ SberAcademy/static/js/test.js
@@ -0,0 +1,39 @@
+function request(type, url, data){
+    let returnData = ''
+    $.ajax({
+        type: type,
+        url: url,
+        data: data,
+        success: function(data) { 
+            if(type == 'GET'){
+                returnData = data
+            }
+        },
+        async: false
+    })
+    return returnData
+}
+
+getTests()
+
+function getTests() {
+    let data = request('GET', '/test/get_tests')
+    let testContainer = $('.test-cards')
+    let threeContainer = ''
+    for (let index = 0; index < data.length; index++) {
+        let test = $("<div class='test'></div>")
+        test.append($("<p class='test-name'></p>").text(data[index].name))
+        test.append($("<p class='test-description'></p>").text(data[index].description))
+        if(index % 3 == 0){
+            threeContainer = $("<div class='container-three'></div>")
+            threeContainer.append(test)
+            testContainer.append(threeContainer)
+        }
+        else{
+            threeContainer.append(test)
+        }
+        if(index + 1 == data.length && index + 1 % 3 != 0){
+            testContainer.append(threeContainer)
+        }
+    }
+}
